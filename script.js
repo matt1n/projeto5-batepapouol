cacf2a196e9db827e440aee21a7fa511fa47412b
Mateus Macêdo <mateusamacedo5@gmail.com>
2022-08-25 19:25:29 -0300

    Rolagem automática concluída

--- script.js
+++ script.js
@@ -4,25 +4,32 @@ document.addEventListener("keypress", function(e) {
         botao.click()
     }
 })
+
 function haha(){
     alert('HAHAHAHAHAHAHAAHAHAHAHAHAHAHAAH CORINGUEI')
 }
+
 const nome = prompt("Qual o seu nome")
 const main = document.querySelector('.main')
 let texto = document.querySelector('.texto')
 
 function mensagem() {
-    if (texto.value!=="")
-    main.innerHTML += `<li><div class="mensagem">
-    <span ><strong>${nome}</strong> para <strong>Todos</strong>: ${texto.value}</span>
-</div></li>`
+    if (texto.value!==""){
+        main.innerHTML += `<li><div class="mensagem">
+            <span ><strong>${nome}</strong> para <strong>Todos</strong>: ${texto.value}</span>
+        </div></li>`
     texto.value=""
-}
+    let ultimaMensagem = main.children.length-1
+    let novaMensagem = main.children[ultimaMensagem]
+    novaMensagem.scrollIntoView();
+    }
 
+}
 function mensagemReservada() {
-    if (texto.value!=="")
-    main.innerHTML += `<li><div class="mensagem reservado">
-    <span ><strong>${nome}</strong> para <strong>Todos</strong>: ${texto.value}</span>
-</div></li>`
-    texto.value=""
+    if (texto.value!==""){
+        main.innerHTML += `<li><div class="mensagem reservado">
+            <span ><strong>${nome}</strong> para <strong>Todos</strong>: ${texto.value}</span>
+        </div></li>`;
+    texto.value="";
+    }
 }
