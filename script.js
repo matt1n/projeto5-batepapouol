6e1208e70b5de0f76618854cc0c9a1e447620d34
Mateus Macêdo <mateusamacedo5@gmail.com>
2022-08-25 17:09:38 -0300

    Layout e começo de criação de mensagens

---
+++ script.js
@@ -0,0 +1,28 @@
+document.addEventListener("keypress", function(e) {
+    if(e.key === "Enter"){
+        const botao = document.querySelector('.icon')
+        botao.click()
+    }
+})
+function haha(){
+    alert('HAHAHAHAHAHAHAAHAHAHAHAHAHAHAAH CORINGUEI')
+}
+const nome = prompt("Qual o seu nome")
+const main = document.querySelector('.main')
+let texto = document.querySelector('.texto')
+
+function mensagem() {
+    if (texto.value!=="")
+    main.innerHTML += `<li><div class="mensagem">
+    <span ><strong>${nome}</strong> para <strong>Todos</strong>: ${texto.value}</span>
+</div></li>`
+    texto.value=""
+}
+
+function mensagemReservada() {
+    if (texto.value!=="")
+    main.innerHTML += `<li><div class="mensagem reservado">
+    <span ><strong>${nome}</strong> para <strong>Todos</strong>: ${texto.value}</span>
+</div></li>`
+    texto.value=""
+}
