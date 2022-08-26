1fb5bbbf9d6022a5cc8e3416205f184ee123b3b3
Mateus Macêdo <mateusamacedo5@gmail.com>
2022-08-26 12:09:58 -0300

    entrar na sala com nome concluido

--- script.js
+++ script.js
@@ -9,9 +9,30 @@ function haha(){
     alert('HAHAHAHAHAHAHAAHAHAHAHAHAHAHAAH CORINGUEI')
 }
 
-const nome = prompt("Qual o seu nome")
+let nome = prompt("Qual o seu nome")
 const main = document.querySelector('.main')
 let texto = document.querySelector('.texto')
+
+//---------------------------API------------------------------
+
+let dadoEntrar = {name: nome}
+function entrar() {
+const promessaEntrar = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', dadoEntrar)
+promessaEntrar.then(sucesso)
+promessaEntrar.catch(erro)}
+entrar()
+function sucesso(resposta){
+    console.log('foi')
+}
+function erro(resposta){
+    if (resposta.response.status===400) {
+        nome = prompt("Qual o seu nome")
+        dadoEntrar = {name: nome}
+        entrar()
+    }
+} 
+
+//---------------------------API------------------------------
 
 function mensagem() {
     if (texto.value!==""){
@@ -28,7 +49,7 @@ function mensagem() {
 function mensagemReservada() {
     if (texto.value!==""){
         main.innerHTML += `<li><div class="mensagem reservado">
-            <span ><strong>${nome}</strong> para <strong>Todos</strong>: ${texto.value}</span>
+            <span ><strong>${nome}</strong> reservadamente para <strong>Todos</strong>: ${texto.value}</span>
         </div></li>`;
     texto.value="";
     }
